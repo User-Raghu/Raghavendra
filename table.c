7c1c8f9bf13e9c1d10ec190bed59c7497623c012
User-Raghu <95925707+User-Raghu@users.noreply.github.com>
2021-12-21 22:24:11 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int n,i,tab;
+  printf("the value of n :");
+   scanf("%d",&n);
+   
+  for(i=1;i<=n;i++)
+  { 
+    tab=i*1;
+  	printf(" %d x 1 = %d",i,tab);
+    
+  	
+  }
+  
+ 
+}
+
